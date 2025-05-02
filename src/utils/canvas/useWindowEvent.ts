95be2c66b11614f147d8e3de77120d889aa863a6
Shoghi Simon <sms@shoghisimon.ca>
2025-05-02 15:32:39 -0400

    feat: overload useWindowEvent to support additional event types

--- src/utils/canvas/useWindowEvent.ts
+++ src/utils/canvas/useWindowEvent.ts
@@ -31,6 +31,20 @@ import { useEffect } from 'react';
  * });
  * ```
  */
+export function useWindowEvent<K extends keyof WindowEventMap>(
+  type: K,
+  listener: K extends keyof WindowEventMap
+    ? (this: Window, ev: WindowEventMap[K]) => void
+    : (this: Window, ev: CustomEvent) => void,
+  options?: boolean | AddEventListenerOptions,
+): void;
+export function useWindowEvent<K extends string>(
+  type: K,
+  listener: K extends keyof WindowEventMap
+    ? (this: Window, ev: WindowEventMap[K]) => void
+    : (this: Window, ev: CustomEvent) => void,
+  options?: boolean | AddEventListenerOptions,
+): void;
 export function useWindowEvent<K extends string>(
   type: K,
   listener: K extends keyof WindowEventMap
